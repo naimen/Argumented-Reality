4463caa8fa2d390190ddc6c149c54591b16ef44e
Shuo Hua <naimen12@gmail.com>
2016-02-18 10:35:04 +0100

    ex3 init

---
+++ milestone 2/core/src/com/ar/exercise3/Exercise3.java
@@ -0,0 +1,39 @@
+package com.ar.exercise3;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class Exercise3 implements ApplicationListener {
+	
+
+	@Override
+	public void create() {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void render() {
+
+	}
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+}
---
+++ milestone 2/desktop/src/com/ar/exercise3/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.ar.exercise3.desktop;
+
+import com.ar.exercise3.Exercise3;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		System.loadLibrary("opencv_java310");
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 480;
+		config.width = 640;
+		new LwjglApplication(new Exercise3(), config);
+	}
+}
