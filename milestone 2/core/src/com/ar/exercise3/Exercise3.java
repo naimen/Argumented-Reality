02b0acb2954fe3d79e4b07e79cf3c1b89c39adc1
NNibe <nicolai_nibe@hotmail.com>
2016-02-24 15:49:06 +0100

    Contours fixed. Showed on color frame atm.

--- milestone 2/core/src/com/ar/exercise3/Exercise3.java
+++ milestone 2/core/src/com/ar/exercise3/Exercise3.java
@@ -4,6 +4,7 @@ import com.ar.util.UtilAR;
 import com.badlogic.gdx.ApplicationListener;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
@@ -34,20 +35,23 @@ public class Exercise3 implements ApplicationListener {
 
 	@Override
 	public void render() {
-		//turn the readed frame to binary
+		//turn the read frame to binary
 		boolean frameRead = cam.retrieve(frame);
 		Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
 		Imgproc.threshold(grayFrame,binaryFrame,123,255,Imgproc.THRESH_BINARY);
 
 		//find contours
-		Imgproc.findContours(binaryFrame.clone(),contours,new Mat(),Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE);
+		contours = new ArrayList<MatOfPoint>();
+		Imgproc.findContours(binaryFrame.clone(), contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 
-
+		//UtilAR.imShow(binaryFrame);
+		Mat contourFrame = binaryFrame.clone();
+		
 		if(!cam.isOpened()){
 			System.out.println("Error");
 		}else if (frameRead){
-			//Imgproc.drawContours(binaryFrame,contours,0,new Scalar(255,0,0));
-			UtilAR.imDrawBackground(binaryFrame);
+			Imgproc.drawContours(frame, contours, -1, new Scalar(0,0,255), 2);
+			UtilAR.imDrawBackground(frame);
 		}
 	}
 	@Override
