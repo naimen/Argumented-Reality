d0a3b42798b1ff317c3d9db6b7274fbec57d0dfe
Nicolai <nicolai_nibe@hotmail.com>
2016-02-24 22:21:06 +0100

    Line draws on polygons Changed a var name

--- milestone 2/core/src/com/ar/exercise3/Exercise3.java
+++ milestone 2/core/src/com/ar/exercise3/Exercise3.java
@@ -41,31 +41,35 @@ public class Exercise3 implements ApplicationListener {
 		//find contours
 		contours = new ArrayList<MatOfPoint>();
 		Imgproc.findContours(binaryFrame.clone(), contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
-		//UtilAR.imShow(binaryFrame);
-		MatOfPoint2f polyFrame = new MatOfPoint2f();
-
-		for(int i =0;i<contours.size();i++)
+		
+		
+		MatOfPoint bestMarker = null;
+		
+		MatOfPoint2f approxPoly = new MatOfPoint2f();
+		for(int i=0; i<contours.size(); i++)
 		{
 			MatOfPoint2f coutourMat = new MatOfPoint2f(contours.get(i).toArray());
-			Imgproc.approxPolyDP(coutourMat,polyFrame,Imgproc.arcLength(coutourMat,true)*0.02,true);
+			Imgproc.approxPolyDP(coutourMat, approxPoly, Imgproc.arcLength(coutourMat,true)*0.02, true);
 			//filter polygons
 
-			MatOfPoint polyFrame2 = new MatOfPoint(polyFrame.toArray());
-			if(polyFrame2.total()==4 &&
+			MatOfPoint approxPoly2 = new MatOfPoint(approxPoly.toArray());
+			if(approxPoly2.total()==4 &&
 					Math.abs(Imgproc.contourArea(coutourMat))>1000 &&
-					Imgproc.isContourConvex((polyFrame2)))
+					Imgproc.isContourConvex((approxPoly2)))
 			{
-				System.out.println(polyFrame2.rows());
-				Imgproc.rectangle(frame,new Point(polyFrame2.get(0,0)),new Point(polyFrame2.get(2,0)),new Scalar(0,255,0),5);
+				Imgproc.line(frame, new Point(approxPoly2.get(0, 0)), new Point(approxPoly2.get(1, 0)), new Scalar(0,255,0), 2);
+				Imgproc.line(frame, new Point(approxPoly2.get(1, 0)), new Point(approxPoly2.get(2, 0)), new Scalar(0,255,0), 2);
+				Imgproc.line(frame, new Point(approxPoly2.get(0, 0)), new Point(approxPoly2.get(3, 0)), new Scalar(0,255,0), 2);
+				Imgproc.line(frame, new Point(approxPoly2.get(2, 0)), new Point(approxPoly2.get(3, 0)), new Scalar(0,255,0), 2);
 
 			}
 		}
+		
 
 		if(!cam.isOpened()){
 			System.out.println("Error");
 		}else if (frameRead){
-			Imgproc.drawContours(frame, contours, -1, new Scalar(0,0,255), 2);
+			//Imgproc.drawContours(frame, contours, -1, new Scalar(0,0,255), 2);
 			UtilAR.imDrawBackground(frame);
 
 		}
